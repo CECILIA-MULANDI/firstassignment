221afea53eace00c1af63c7ee6c67808d1589d5c
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2022-06-25 14:41:29 +0300

    changes

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    let mytext = document.querySelector("#mytext");
+    let msg = document.querySelector("#msg");
+    msg.innerHTML = "Hello " + mytext.value + " welcome to the bootcamp"
+
+}
